0b1741b3fa1c2ada1d773a49f2f1fba8d3a5a42b
ani67h <ghoshanindya4589@gmail.com>
2025-05-17 21:04:21 +0600

    dustu

---
+++ datatypes.py
@@ -0,0 +1,13 @@
+# Let's check the datatype of diffenrent values
+
+a = 5
+print("Type of a : ", type(a))
+
+b = 2.5
+print("Type of b : ", type(b))
+
+c ="coding"
+print("Type of c : ", type(c))
+
+d = True
+print("Type of d : ", type(d))
---
+++ stringoperation.py
@@ -0,0 +1,4 @@
+Str = "string"
+print(Str[::3])
+print(Str[0::1])
+print(Str[::-1])
