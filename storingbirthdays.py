7358e73bdf69b3faa8b730b92a29de8a737c2876
ani67h <ghoshanindya4589@gmail.com>
2025-05-22 21:37:40 +0600

    happybirthday

---
+++ storingbirthdays.py
@@ -0,0 +1,36 @@
+print('Me : Hey, what is your name?')
+print('Petter : Petter.')
+x = 'Petter'
+print('Me : When is your birthday?')
+print('Petter : 12th September.')
+y = "12th September"
+print('Me : Great to know that ' + x + ', your brthday is in ' + y + '.' )  
+
+print("Me : Hello, Jane what's up?")
+print('Jane : Nothing much. What about you?')
+print('Me : Yeah, so far so good. I need your help.')
+print('Jane : About what?')
+print('Me : So basically, I am collecting people birthday. Can you tell me when is your birthday?')
+print("Jane : Ah... it's in today.")
+z = 'day'
+u = 'Jane'
+print('Me : Really! Happy birthday ' + u + '! Have a great ' + z)
+print('Jane : Thank you so much!')
+
+print('Me : Hi Sara, how are you doing?')
+print('Sara : Yep, everything is ok.')
+print("Me : So Sara I want to ask your birth date so, I don't forget presents.")
+print('Sara : Glad to know that. My birthday is in 1st February.')
+s = "Sara"
+a = "A special gift will be waiting for you on that day."
+print("Me : Thank you very much " + s + "!" + a)
+print("Sara : I can't wait for that day!")
+print('Me : Hold your horses, it will be good.')
+
+print('Me : Hello Steve, can I know your birthday?')
+print('Steve : Huh?')
+print('Me : Your birth date.')
+print("Steve : Who is Steve he died before birth...bye.")
+d = 'died'
+e = 'bye'
+print("Me : Glad to know that you " + d + "..." + e + "!")
