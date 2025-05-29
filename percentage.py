61f2aa367b37f2d4df694552cfb95a3a91f21688
ani67h <ghoshanindya4589@gmail.com>
2025-05-29 22:27:31 +0600

    cuberoot

---
+++ percentage.py
@@ -0,0 +1,16 @@
+m = int(input('Enter your percentage in Maths '))
+p = int(input('Enter your perentage in Physics '))
+c = int(input('Enter your percentage in Chemistry '))
+b = int(input('Enter your percentage in Biology ')) 
+e = int(input('Enter your percentage in English '))
+ec = int(input('Enter your percentage in Economics '))
+a = int(input('Enter your percentage in Accounting '))
+bs = int(input('Enter your percentage in Business Studies '))
+ad = int(input('Enter your percentage in Art and Design '))
+cs = int(input('Enter your percentage in Computer Science '))
+
+sum = m+p+c+b+e+ec+a+bs+ad+cs
+avg = sum/10
+
+print("You got " ,avg, " % in the exam." )
+print("Remember grades are just number infront of your dreams and passion. Do not study to achieve highest grades in your exams; study to learn. At first, it would be very weird, but your dreams and passion of something might change the world - who knows!")
---
+++ root.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number to know it's root : "))
+root = num**0.5
+
+print("The root of ",num, "is ",root)
+
+num1 = int(input("Enter a number to know it's cuberoot : "))
+cubero = num1**0.666666666666666667
+cuberoot = cubero**0.5
+#Here 0.666667 can be writen as, 2/3 and 0.5 can be writen as 1/2. If we multiply, 2/3 and 1/2 then we will get 1/3, which ultimately gives perfect cuberoots.
+
+print("The root of ",num1, "is ",cuberoot)
