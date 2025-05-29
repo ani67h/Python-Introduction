146073e8be82b986950e93f48ddf0172aba466c8
ani67h <ghoshanindya4589@gmail.com>
2025-05-29 22:57:27 +0600

    weather

---
+++ summertimes.py
@@ -0,0 +1,10 @@
+temp = int(input('Enter the current temperature (in degree celcius) here : '))
+
+if (temp < 15 ):
+    print("It is ❄️ Winter season. Winter is cold. Wear warm clothes, eat hot food, and avoid staying wet or skipping meals.")
+elif (temp < 25):
+    print("It is 🌸 Spring season. Spring is cool and pleasant. Flowers bloom, and the weather is perfect for going outside. Allergy care may be needed.")    
+elif (temp < 30):
+    print("It is 🌤️ Early Summer season. The weather gets warmer. Light clothes and more water help prepare for hotter days ahead.")
+else:
+    print("It is 🌞 Summer season. Summer is hot. Stay cool with water, light clothes, and shade. Avoid heavy food and sun exposure.")    
