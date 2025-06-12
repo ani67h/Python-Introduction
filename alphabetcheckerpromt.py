9c3089c087ef8aa8f5cac48e34ae90ac377060d9
ani67h <ghoshanindya4589@gmail.com>
2025-06-12 17:39:52 +0600

    alphabetpromt

---
+++ alphabetcheckerpromt.py
@@ -0,0 +1,8 @@
+print("Enter a alphabet : ", end="")
+n = input()
+if n == "a" or "b" or "c" or "d" or "e" or "f" or "g" or "h" or "i" or "j" or "k" or "l" or "m" or "n" or "o" or "p" or "q" or "r" or "s" or "t" or "u" or "v" or "w" or "x" or "y" or "z":
+    print(n," is an alphabet.")
+elif n == "A" or "B" or "C" or "D" or "E" or "F" or "G" or "H" or "I" or "J" or "K" or "L" or "M" or "N" or "O" or "P" or "Q" or "R" or "S" or "T" or "U" or "V" or "W" or "X" or "Y" or "Z":
+    print(n," is an alphabet.")
+else:
+    print("Please, enter a valid alphabet.")
